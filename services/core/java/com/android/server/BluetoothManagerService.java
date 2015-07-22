48181d7ce877f51953cadf9d65d3afc34dfe07f7
Nitin Arora <niarora@codeaurora.org>
2015-07-22 15:54:30 -0700

    Bluetooth: Send Bluetooth Off intent based in BLEApp count
    This change checks for the precense of an Always On
    APP while moving Bluetooth state from ON to BLE_ON
    state. If there is an app present, BT_OFF intent is
    immidiately sent, else the intent is sent when BT is
    completely turned OFF. This prevents the intermediate
    state where BT Off intent, enables the BT toggle switch
    while BT core services are still turning off.
    
    Change-Id: I169af5a9aedd18e38ad990a793a437ac40cbe9ef

--- services/core/java/com/android/server/BluetoothManagerService.java
+++ services/core/java/com/android/server/BluetoothManagerService.java
@@ -145,6 +145,7 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
     private final BluetoothHandler mHandler;
     private int mErrorRecoveryRetryCounter;
     private final int mSystemUiUid;
+    private boolean mIntentPending = false;
 
     // Save a ProfileServiceConnections object for each of the bound
     // bluetooth profile services
@@ -1663,7 +1664,11 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                         unbindAndFinish();
                         sendBleStateChanged(prevState, newState);
                         // Don't broadcast as it has already been broadcast before
-                        isStandardBroadcast = false;
+                        if(!mIntentPending) {
+                            isStandardBroadcast = false;
+                        } else {
+                            mIntentPending = false;
+                        }
                     }
 
                 } else if (!intermediate_off) {
@@ -1692,6 +1697,13 @@ class BluetoothManagerService extends IBluetoothManager.Stub {
                     // Broadcast as STATE_OFF
                     newState = BluetoothAdapter.STATE_OFF;
                     sendBrEdrDownCallback();
+                    if(!isBleAppPresent()){
+                        isStandardBroadcast = false;
+                        mIntentPending = true;
+                    } else {
+                        mIntentPending = false;
+                        isStandardBroadcast = true;
+                    }
                 }
             } else if (newState == BluetoothAdapter.STATE_ON) {
                 boolean isUp = (newState==BluetoothAdapter.STATE_ON);
